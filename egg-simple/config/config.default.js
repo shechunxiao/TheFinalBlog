5ba1e155e0b1a27eea6a99cdb983181033f64991
282074579@qq.com <282074579@qq.com>
2020-09-18 22:35:07 +0800

    log

--- egg-simple/config/config.default.js
+++ egg-simple/config/config.default.js
@@ -14,7 +14,13 @@ module.exports = appInfo => {
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1600324030541_9587';
-
+  config.cluster = {
+    listen :{
+      port: 3000,
+      hostname: '127.0.0.1', // 不建议设置 hostname 为 '0.0.0.0'，它将允许来自外部网络和来源的连接，请在知晓风险的情况下使用
+      // path: '/var/run/egg.sock',
+    }
+  };
   // add your middleware config here
   config.middleware = ['test'];
   config.test ={
